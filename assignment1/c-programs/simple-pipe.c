62c57ec644dd68740e54f852fff2d5c86d1ca048
Keaton Surfield <75916683+KeatonSurfield@users.noreply.github.com>
2026-06-24 22:56:04 -0400

    Update simple-pipe.c

--- assignment1/c-programs/simple-pipe.c
+++ assignment1/c-programs/simple-pipe.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
 int main() {
-int pipefd[2];
-pid_t pid;
-char buffer[100];
-char *message = "Hello from parent!";
-if (pid == 0) {
-} else {
-}
-return 0;
+    int pipefd[2];
+    int pid;
+    char buffer[100];
+    char *message = "Hello from parent!";
+    if (pipe(pipefd) == -1) {
+        printf("Error creating pipe\n");
+        return 1;
+    }
+    pid = fork();
+    if (pid == -1) {
+        printf("Error creating child process\n");
+        return 1;
+    }
+    if (pid == 0) {
+        close(pipefd[1]);
+        read(pipefd[0], buffer, 100);
+        printf("Child received: %s\n", buffer);
+        close(pipefd[0]);
+    }
+    else {
+        close(pipefd[0]);
+        write(pipefd[1], message, 19);
+        close(pipefd[1]);
+        wait(NULL);
+    }
+    return 0;
 }
