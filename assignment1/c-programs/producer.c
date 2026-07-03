77d206fce37a8df8bc8cf881ef7af46b2a8ac868
Keaton Surfield <75916683+KeatonSurfield@users.noreply.github.com>
2026-07-03 11:32:46 -0400

    Fork producer.c

--- assignment1/c-programs/producer.c
+++ assignment1/c-programs/producer.c
@@ -1,109 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <signal.h>
-
-volatile sig_atomic_t shutdown_flag = 0;
-
-long bytes_sent = 0;
-
-void handle_sigint(int sig) {
-    shutdown_flag = 1;
-}
-
-void handle_sigusr1(int sig) {
-    fprintf(stderr,
-            "\nProducer Statistics\nBytes sent: %ld\n",
-            bytes_sent);
-}
-
-int main(int argc, char *argv[]) {
-    int opt;
-    char *filename = NULL;
-    int buffer_size = 4096;
-
-    struct sigaction sa_int;
-    struct sigaction sa_usr1;
-
-    sa_int.sa_handler = handle_sigint;
-    sigemptyset(&sa_int.sa_mask);
-    sa_int.sa_flags = 0;
-    sigaction(SIGINT, &sa_int, NULL);
-
-    sa_usr1.sa_handler = handle_sigusr1;
-    sigemptyset(&sa_usr1.sa_mask);
-    sa_usr1.sa_flags = 0;
-    sigaction(SIGUSR1, &sa_usr1, NULL);
-
-    while ((opt = getopt(argc, argv, "f:b:")) != -1) {
-        switch (opt) {
-            case 'f':
-                filename = optarg;
-                break;
-
-            case 'b':
-                buffer_size = atoi(optarg);
-                break;
-
-            default:
-                fprintf(stderr,
-                        "Usage: %s [-f file] [-b size]\n",
-                        argv[0]);
-                return 1;
-        }
-    }
-
-    FILE *input;
-
-    if (filename != NULL) {
-        input = fopen(filename, "r");
-
-        if (input == NULL) {
-            fprintf(stderr,
-                    "Could not open file %s\n",
-                    filename);
-            return 1;
-        }
-    }
-    else {
-        input = stdin;
-    }
-
-    char *buffer = malloc(buffer_size);
-
-    if (buffer == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-
-        if (filename != NULL) {
-            fclose(input);
-        }
-
-        return 1;
-    }
-
-    size_t bytes_read;
-
-    while (!shutdown_flag &&
-           (bytes_read = fread(buffer,
-                               1,
-                               buffer_size,
-                               input)) > 0) {
-
-        fwrite(buffer, 1, bytes_read, stdout);
-
-        bytes_sent += bytes_read;
-    }
-
-    fprintf(stderr,
-            "\nProducer exiting\nBytes sent: %ld\n",
-            bytes_sent);
-
-    free(buffer);
-
-    if (filename != NULL) {
-        fclose(input);
-    }
-
-    return 0;
-}
+#ifndef BUFFER_H
+#define BUFFER_H
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#define SHM_KEY       0x1234
+#define BUFFER_SIZE   10
+#define SEM_EMPTY "/sem_empty"
+#define SEM_FULL  "/sem_full"
+#define SEM_MUTEX "/sem_mutex"
+typedef struct
+{
+    int value;         
+    int producer_id;   
+} item_t;
+typedef struct
+{
+    item_t buffer[BUFFER_SIZE];
+    int head;          
+    int tail;          
+    int count;          
+} shared_buffer_t; 
+int create_shared_memory(void);
+int open_shared_memory(void);
+shared_buffer_t *attach_shared_memory(int shmid);
+void detach_shared_memory(shared_buffer_t *buffer);
+sem_t *open_empty_semaphore(void); //semaphore for multithreading
+sem_t *open_full_semaphore(void);
+sem_t *open_mutex_semaphore(void);
+#endif
